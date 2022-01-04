5db92f5639187b8e5a5422299b55c17c0b7dce46
SUSHANT KUMAR <97085584+sushantsingh24@users.noreply.github.com>
2022-01-04 14:09:02 +0530

    my second program in C
    my second program in  C and in this program only for add.... ,subt..., divide , multipication.

---
+++ second.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+
+
+     printf("enter the value of a\n");
+         
+         scanf("%d",&a);
+
+     printf("enter the value of b\n");
+       
+       scanf("%d",&b);
+    
+       printf("sum the value of %d\n ",a+b);
+       printf("subt the value of %d\n ",a-b);
+       printf("multiply the value of %d\n",a*b);
+       printf("divide the value of %d\n",a/b);
+
+    return 0 ;
+}
